33501375b31f3fe33ba15fe1e819decf7744d6df
brachalevi <brachalevi@hilma.tech>
2023-10-15 18:06:57 +0300

    i creat the name of fue function(not finish)

--- JS/gameplay.js
+++ JS/gameplay.js
@@ -4,3 +4,28 @@ const orders=[
         order:['bottom-bun', 'tomato']
     }
 ]
+
+let order_bank = [['bottom-bun','tomato','onion','patty','lettuce','top-bun'],['bottom-bun','patty','top-bun'],['bottom-bun','patty','tomato','onion','top-bun']];
+const order_call = []
+
+/*still not finish*/
+function fillOrder(){
+    if 
+
+}
+/*still not finish*/
+function checkOrder(order){
+    for (let i = 0; i < order_call.length; i++) {
+        if(order==order_call[i]){
+            /*add money*/
+            orders.splice(i, 1);
+            fillOrder();
+            return 0;
+        }
+    }
+
+}
+
+
+
+
